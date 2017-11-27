0ba2cd2c6609bfcf30249f8d82c50a55a6092462
ShangYunliang <1071332303@qq.com>
2017-11-27 18:50:41 +0800

    finish lesson2

--- src/App.js
+++ src/App.js
@@ -1,43 +1,59 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { compose, setDisplayName, setPropTypes } from 'recompose'
-const { connect } = Redux()
+import { compose, withState, withHandlers } from 'recompose'
+import './App.css'
 
-const enhance = compose(
-  setDisplayName('User'),  // mast be here  顺序先执行 connect 在执行 setPropTypes 最后执行 setDisplayName
-  setPropTypes({
-    name: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-  }),
-  connect(),
+const withToggle = compose(
+  withState('toggledOn', 'toggle', false),
+  withHandlers({
+    show: ({ toggle }) => (e) => toggle(true),
+    hide: ({ toggle }) => (e) => toggle(false),
+    toggle: ({ toggle }) => (e) => toggle((current) => !current),
+   })
 )
 
-const User = enhance(({name, status, dispatch}) => (
-  <div onClick = {
-    () => dispatch({type: 'USER_ACTIVE'})
-  }>
-  {name}: {status}
-  </div>
-))
+const StatusList = () =>
+  <div className='StatusList' >
+   <div>pending </div>
+   <div>inactive </div>
+   <div>active </div>
+  </div>;
+//
+// const Status = withState('listShown', 'setListVisible', false)(
+//   ({ status, listShown, setListVisible }) =>
+//  <span onClick={()=> setListVisible((x) => !x)}>
+//    {status}
+//    { listShown && <StatusList /> }
+//  </span>
+// );
 
-console.log(User.displayName);
+const Status = withToggle(({ status, toggledOn, toggle }) =>
+<span onClick={toggle}>
+ {status}
+ { toggledOn && <StatusList /> }
+</span>)
 
-const App = () =>
-<div>
- <User status='active' />
-</div>
+// const Tooltip = withState('tooltipShown', 'setTooltipVisible', false)(
+//   ({ text, children, tooltipShown, setTooltipVisible }) =>
+//   <span>
+//    { tooltipShown && <div className = 'Tooltip'>{ text } </div>}
+//    <span onMouseEnter={() => setTooltipVisible(true)}
+//    onMouseLeave={() => setTooltipVisible(false)}>{children}</span>
+//   </span>
+// )
+//
+
+const Tooltip = withToggle(({ text, children, toggledOn, show, hide }) =>
+  <span>
+   { toggledOn && <div className = 'Tooltip'>{ text } </div>}
+   <span onMouseEnter={show} onMouseLeave={hide}>{children}</span>
+  </span>)
+
+const User = ({ name, status }) =>
+  <div className='User'>
+    <Tooltip text='Cool Dude'>{name}</Tooltip>---
+    <Status status={status} />
+  </div>;
+
+const App = () => <User name='SYL' status='ACTIVE' />
 
 export default App
-
-
-// fake implementation of redux
-
-function Redux() {
-  return {
-    connect: () => (BaseComponent) => (props) =>
-      <BaseComponent
-        {...props}
-        dispatch={ ({ type }) => console.log(type + ' dispatched') }
-      />
-  }
-}
